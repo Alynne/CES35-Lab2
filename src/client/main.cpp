e382556e724585353a2c4b53af1ce86d7523884d
alynne <alynnemaj@gmail.com>
2020-10-17 20:30:57 -0300

    client integration part 1

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
+#include "client.h"
+
 int main(int argc, char* argv[]) {
+    
+    
     if (argc < 2) { // We expect at least one URL
-        std::cerr << "Error: At least one URL expected" << std::endl;
+        std::cerr << "Error: At least one URL expected." << std::endl;
         return 1;
     }
     for (int i = 1; i < argc; i++){
+        auto url = http::url::parse(argv[i]);
         
+        if (url.has_value()){
+            if((url->getScheme() == "http") || (url->getScheme() == "https")){
+                std::string host = std::string(url->getHost());
+                std::uint16_t port = url->port;
+                http_client myClient(host, port);
+            }
+            else{
+                std::cerr<< "Error: Not http."<< std::endl;
+            }
+
+        }
+        else {
+            std::cerr<< "Error: Unable to parse URL."<< std::endl;
+        }
     }
     return 0;
 }
